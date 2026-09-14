6be234228eb06b9c19e9babc5dfabb5321e96dec
Bhavishya Gupta <futureji2025@gmail.com>
2026-09-14 08:05:53 +0530

    feat: created category endpoints

--- Program.cs
+++ Program.cs
@@ -15,13 +15,31 @@ if (app.Environment.IsDevelopment())
     app.MapScalarApiReference();
 }
 
-app.MapGet("/products", async (AppDbContext db) => await db.products.ToListAsync());
+app.MapGet("/products", async (AppDbContext db) => await db.Products.ToListAsync());
+
+app.MapGet("/categories", async (AppDbContext db) =>
+    await db.Categories
+    .Select(c => new { c.Id, c.Name })
+    .ToListAsync());
+
+app.MapGet("/categories/{id}/products", async (AppDbContext db, int id) =>
+{
+    return await db.Products.Where(p => p.CategoryId == id)
+    .ToListAsync();
+});
 
 app.MapPost("/products", async (Product product, AppDbContext db) =>
 {
-    db.products.Add(product);
+    db.Products.Add(product);
     await db.SaveChangesAsync();
     return Results.Created($"/products/{product.Id}", product);
+});
+
+app.MapPost("/categories", async (Category category, AppDbContext db) =>
+{
+    db.Categories.Add(category);
+    await db.SaveChangesAsync();
+    return Results.Created($"/categories/{category.Id}", category);
 });
 
 app.UseHttpsRedirection();
